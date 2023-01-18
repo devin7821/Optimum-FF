fe380c2a7f4ae220347601054da23e904654e97f
devin782 <devin.wolford@oit.edu>
2023-01-17 16:04:13 -0800

    New gitignore and removed dirt files.

---
+++ Optimum FF/Optimum FF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Optimum_FF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Current.MainWindow = this;
+            Loaded += OnMainMenuLoaded;
+        }
+
+        private void OnMainMenuLoaded(object sender, RoutedEventArgs e)
+        {
+            ChangeView(new MainMenu());
+        }
+
+        public void ChangeView(Page view)
+        {
+            mainFrame.NavigationService.Navigate(view);
+        }
+    }
+}
